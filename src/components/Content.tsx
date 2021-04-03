acf116fb81da599e6bbf1285b58dae3c526b8787
Caroline Carvalho <carvalhocaroline111@gmail.com>
2021-04-03 14:59:41 -0300

    Conclui desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import { GenreResponseProps } from './SideBar'
+
+import './../styles/content.scss';
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: {selectedGenre: GenreResponseProps, movies: MovieProps[]}) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
